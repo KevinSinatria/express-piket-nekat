a6ecb75d8a6f7778bf3b3fc3ab586b5ff4e1e1fb
Agung <agungcoygamez123@gmail.com>
2025-12-12 22:42:19 +0700

    all of subjects features has completed

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import AuthRoutes from "./modules/auth/auth.routes.js";
 import UsersRoutes from "./modules/users/user.routes.js";
+import SubjectRoutes from "./modules/subject/subject.routes.js";
 
 const router = Router();
 
 router.use("/auth", AuthRoutes);
 router.use("/users", UsersRoutes);
+router.use("/subjects", SubjectRoutes);
 
 export default router;
